3cb65ac03bc680f490e535bd46ccd089aeb7d1d5
Dylan <93456651+DKane6112@users.noreply.github.com>
2023-12-26 13:37:38 +0000

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static int[] test(int[] notes, int start) {
+        int[] test = new int[7];
+
+
+        for(int i = 0; i < notes.length; i++){
+            test[i] = notes[start];
+            start = start + 1;
+            if(start == notes.length){
+                start = 0;
+            }
+        }
+        return test;
+    }
+    public static void main(String[] args) {
+        int[] notes = {1,2,3,4,5,6,7,};
+        int[] test = test(notes,2);
+        for(int i = 0; i <test.length; i++){
+            System.out.println(test[i]);
+        }
+
+        }
+    }
