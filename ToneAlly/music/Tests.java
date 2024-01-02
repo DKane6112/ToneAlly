201c3fcf86d075f11a8a5845f6a853bd706b59a2
Dylan <93456651+DKane6112@users.noreply.github.com>
2024-01-02 16:57:11 +0000

    Update Tests.java

--- ToneAlly/music/Tests.java
+++ ToneAlly/music/Tests.java
@@ -1,5 +1,10 @@
 package music;
 
+import music.Notes.*;
+import java.util.Scanner;
+import static music.Notes.*;
+import static music.Main.*;
+
 Public class Tests
 {
   public static String inputString (String message)
@@ -15,11 +20,11 @@ Public class Tests
 
   public static void sortByKeyTest()
   {
-    String [] allNotes = Chromatic.getAllNotes();
+        String [] allNotes = Chromatic.getAllNotes();
         String userKey = inputString("Choose your key: ");
         int index = Chromatic.getIndex(userKey);
 
-        String [] test = test2(allNotes, index);
+        String [] test = sortByKey(allNotes, index);
         for(int i = 0; i <test.length; i++)
         {
             System.out.println(test[i]);
