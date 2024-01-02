df5cb35058f03085b1263415b29bab5d6230dc2f
Dylan <93456651+DKane6112@users.noreply.github.com>
2024-01-02 16:46:12 +0000

    Update Main.java
    Changed some variable and method names to be more descriptive

--- ToneAlly/music/Main.java
+++ ToneAlly/music/Main.java
@@ -7,20 +7,20 @@ import static music.Notes.*;
 public class Main 
 {
     // This method will be what is used to change the array of all music notes to start at the key the user provides
-   public static String[] test(String[] notes, int start) 
+   public static String[] sortByKey(String[] notes, int start) 
     {
         int length = notes.length;
-        String[] test = new String[length];
+        String[] notesByKey = new String[length];
 
         for(int i = 0; i < notes.length; i++)
         {
-            test[i] = notes[start];
+            notesByKey[i] = notes[start];
             start = start + 1;
             if(start == notes.length){
                 start = 0;
             }
         }
-        return test;
+        return notesByKey;
     }
 
     public static String [] majorScale(String text)
